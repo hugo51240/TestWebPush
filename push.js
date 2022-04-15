4a8ce492a542c02d9db3dff96d94d930ad185418
Patrice Adam <p.adam@infosoft.com>
2022-04-15 11:19:40 +0200

    modif sw

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKey = {
 
 push.setVapidDetails('mailto:test@test.com', vapidKey.publicKey, vapidKey.privateKey);
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/f7xzQNCH5bo:APA91bGTNo1DdsBU9CU0lfIAKf0e6aNPWMRfQslZEesYc6j0yJBxKYJknfDJNbFgo1mR3emdyyBUsed5CpDgja2_I8L0laDlXCZntLSf-_q0rMlJPODEacAzKYKSfZOGMU5SfklMRP-I",
+    expirationTime:null,
+    keys: {
+        p256dh:"BEuYqEMU6UPMXGR0NrKDgbCY8JzwHyXscxD27BJWbbxUoDwAmFS-kRJSudxzadXmj_Gh7GSRnTbDaIFd7UnDzXc",
+        auth:"nMBjwsuqvKqCk3UwRe6jJw"
+    }
+};
 
 push.sendNotification(sub, 'test message');
