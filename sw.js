b6c6dae0851a3cdac0ad3c165633414b4c72b36a
Patrice Adam <p.adam@infosoft.com>
2022-04-21 14:52:56 +0200

    fix manifest

--- sw.js
+++ sw.js
@@ -1,3 +1,56 @@
+const CACHE_NAME = 'test-v1';
+const CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.webmanifest',
+  '/push.js',
+  '/images/pwa-48x48.png',
+  '/images/pwa-192x192.png',
+  '/images/pwa-512x512.png',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] installation');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Mise en cache du contenu');
+      return cache.addAll(CACHE);
+    })
+  );
+});
+
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Récupération de la ressource: ' + e.request.url);
+      return r || fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Mise en cache de la nouvelle ressource: ' + e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME)
+            return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('push', function (e) {
   var options = {
     body: 'This notification was generated from a push!',
