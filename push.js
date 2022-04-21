ae6b51d250e2b6ade0e9ee666634010816453dd5
Patrice Adam <p.adam@infosoft.com>
2022-04-21 14:05:12 +0200

    ajout action sur click notif

--- push.js
+++ push.js
@@ -3,7 +3,8 @@ var push = require('web-push');
 let vapidKey = {
     publicKey: 'BHA6KBBYvqHcdljFv3OuHBlGMQBkJGaW_eXQz2TzOqlonpkIQNmHq-IOUeE4Sdtv-CoPnH4I50Ir3rfyTLxFF6c',
     privateKey: 'UEtIIRWiVjB10V72coqFSO5SM_iisRTw8-ITz4BaJBk'
-  };
+};
+
 
 /*
 push.setGCMAPIKey('AIzaSyBGbYWQMVNCrIIawj8yDH-LER12j2vIHpY');
@@ -11,11 +12,11 @@ push.setVapidDetails('mailto:test@test.com', vapidKey.publicKey, vapidKey.privat
 */
 
 let sub = {
-    endpoint:"https://fcm.googleapis.com/fcm/send/f7xzQNCH5bo:APA91bGTNo1DdsBU9CU0lfIAKf0e6aNPWMRfQslZEesYc6j0yJBxKYJknfDJNbFgo1mR3emdyyBUsed5CpDgja2_I8L0laDlXCZntLSf-_q0rMlJPODEacAzKYKSfZOGMU5SfklMRP-I",
-    expirationTime:null,
-    keys:{
-        p256dh:"BEuYqEMU6UPMXGR0NrKDgbCY8JzwHyXscxD27BJWbbxUoDwAmFS-kRJSudxzadXmj_Gh7GSRnTbDaIFd7UnDzXc",
-        auth:"nMBjwsuqvKqCk3UwRe6jJw"
+    endpoint: "https://fcm.googleapis.com/fcm/send/f7xzQNCH5bo:APA91bGTNo1DdsBU9CU0lfIAKf0e6aNPWMRfQslZEesYc6j0yJBxKYJknfDJNbFgo1mR3emdyyBUsed5CpDgja2_I8L0laDlXCZntLSf-_q0rMlJPODEacAzKYKSfZOGMU5SfklMRP-I",
+    expirationTime: 5,
+    keys: {
+        p256dh: "BEuYqEMU6UPMXGR0NrKDgbCY8JzwHyXscxD27BJWbbxUoDwAmFS-kRJSudxzadXmj_Gh7GSRnTbDaIFd7UnDzXc",
+        auth: "nMBjwsuqvKqCk3UwRe6jJw"
     }
 };
 
